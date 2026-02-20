64224108bfdcf693d122a851415398e5641a1926
Kavinda Perera <140135039+k4v1nd4-p3r3r4@users.noreply.github.com>
2026-02-20 18:28:06 +0530

    connect mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import path from "path";
+
+dotenv.config({ path: path.resolve("backend", ".env") });
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.info(`Server running on PORT ${PORT}`);
-})
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server running on PORT ${PORT}`);
+});
